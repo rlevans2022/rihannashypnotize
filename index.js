d7fc52c0d4e7fcc5e3a73e1a0dae12320831632e
rlevans2022 <35820954+rlevans2022@users.noreply.github.com>
2018-06-22 11:25:27 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+function myFunction () {
+  var checkBox = document.getElementById('excited');
+  if (checkBox.checked == true){
+    alert ('cool😃');
+    }
+  }
+
+
+  function myFunction2 () {
+    var checkBox = document.getElementById('nervous');
+    if (checkBox.checked == true){
+      alert ('there are easiser ones😇');
+      }
+    }
